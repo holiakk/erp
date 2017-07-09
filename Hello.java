48c530b5cb910cef6225a17500a0738af497cdb2
lazycat <631029386@qq.com>
2017-07-09 13:42:54 +0800

    change Hello.java File

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("nihao, shijie!");
+
+        System.out.println("aaaaaaa");
     }
 }
